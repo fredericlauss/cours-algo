2ed659e442f33da959049160dcf8beb501cd3d22
frederic <flausson@normandiewebschool.fr>
2022-10-04 09:53:59 +0200

    on va plus loin

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 <?php
 
 for ($i = 1; $i <= 100; $i++) {
-    echo $i;
-    echo "\n";
+    if ($i % 2 == 0){
+        $color = 32;
+        echo "\033[" . $color . "m" . $i . "\n";
+      } elseif ($i% 2 == 1) {
+        $color = 34;
+        echo "\033[" . $color . "m" . $i . "\n";
+      }
 }
